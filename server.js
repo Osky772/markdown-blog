cbd10628f38283aa08c76454663e07791d57918d
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-06 17:49:24 +0100

    change: display all posts from db and add button to link to that post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 require('./.db-config');
 
@@ -14,19 +15,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  const articles = [
-    {
-      title: 'Test article',
-      createdAt: new Date(),
-      description: 'test description',
-    },
-    {
-      title: 'Test article 2',
-      createdAt: new Date(),
-      description: 'test description 2',
-    },
-  ];
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' });
   res.render('articles/index', { articles });
 });
 
