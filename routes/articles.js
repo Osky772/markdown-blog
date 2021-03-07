ba0b5ae8663d7eff1e59afd92c1f55e9f1663f33
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-07 16:28:48 +0100

    change: edit post

--- routes/articles.js
+++ routes/articles.js
@@ -17,22 +17,45 @@ router.get('/:slug', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  const { title, description, markdown } = req.body;
-  let article = new Article({
-    title,
-    description,
-    markdown,
-  });
-
-  try {
-    article = await article.save();
-    res.redirect(`/articles/${article.slug}`);
-  } catch (e) {
-    console.log('Error while sending a form: ', e);
-    res.render('articles/new', { article });
-  }
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render('articles/edit', { article });
 });
+
+router.post(
+  '/',
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect('new')
+);
+
+router.put(
+  '/:id',
+  async (req, res, next) => {
+    req.article = await Article.finById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect('edit')
+);
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let { article } = req;
+    const { title, description, markdown } = req.body;
+    article.title = title;
+    article.description = description;
+    article.markdown = markdown;
+
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (error) {
+      res.render(`articles/${path}`, { article, error });
+    }
+  };
+}
 
 router.delete('/:id', async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
