dc45d34d2d69e681100091234fda4022cb0bd4a1
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-07 15:36:47 +0100

    change: add button to delete post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 require('./.db-config');
@@ -15,6 +16,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' });
